5e64e42cbff54c7e8fae166719b31b05d47ab0fe
Peter Labadorf <plaba3.1415@gmail.com>
2018-09-04 19:00:27 -0400

    Pele works now. milkshakes for all involved. RIP Santiago

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,15 +8,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import com.ctre.phoenix.motorcontrol.com.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
@@ -26,16 +21,22 @@ import edu.wpi.first.wpilibj.MecanumDrive;
  */
 public class Robot extends TimedRobot {
   
-  private WPI_TalonSRX m_frontLeft  = new WPI_TalonSRX(0);	
-	private WPI_TalonSRX m_rearLeft   = new WPI_TalonSRX(2);
-	private WPI_TalonSRX m_frontRight = new WPI_TalonSRX(1);	
-  private WPI_TalonSRX m_rearRight  = new WPI_TalonSRX(4);
+  private WPI_TalonSRX m_frontLeft  = new WPI_TalonSRX(4);
+  
+  private WPI_TalonSRX m_rearLeft   = new WPI_TalonSRX(3);
+  private WPI_TalonSRX m_frontRight = new WPI_TalonSRX(2);	
+  private WPI_TalonSRX m_rearRight  = new WPI_TalonSRX(1);
   private MecanumDrive m_robotDrive = new MecanumDrive(m_frontLeft,m_rearLeft,m_frontRight,m_rearRight);
      //Define joystick being used at USB port 1 on the Driver Station
   Joystick m_driveStick = new Joystick(0);
-
+  public void robotInit(){
+     m_frontLeft.setInverted(true);
+  }
      public void teleopPeriodic(){
-          m_robotDrive.driveCartesian(m_driveStick.getX(), m_driveStick.getY(), m_driveStick.getZ());
+          SmartDashboard.putNumber("Joystick X", m_driveStick.getX());
+          SmartDashboard.putNumber("Joystick Y", m_driveStick.getY());
+          SmartDashboard.putNumber("Joystick Z", m_driveStick.getZ());
+          m_robotDrive.driveCartesian(m_driveStick.getX(),-m_driveStick.getY(), m_driveStick.getZ());
      }
   
 }
